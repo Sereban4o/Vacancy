8c69a547975522ac607df32348c407dfa19daa82
User <s.hlopovskih@gmail.com>
2025-11-22 16:49:13 +0300

    Issue 6: Koin modules Добавил основные коин модули (также добавил модуль для конвертеров данных) Перенёс App в отдельную папку application в ui слое. Прописал внутри App инициализацию модулей Koin.

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/application/App.kt
@@ -0,0 +1,31 @@
+package ru.practicum.android.diploma.ui.application
+
+import android.app.Application
+import android.util.Log
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import ru.practicum.android.diploma.di.convertersModule
+import ru.practicum.android.diploma.di.interactorModule
+import ru.practicum.android.diploma.di.networkModule
+import ru.practicum.android.diploma.di.repositoryModule
+import ru.practicum.android.diploma.di.storageModule
+import ru.practicum.android.diploma.di.viewModelModule
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        //место Koin, Room, и т.п.
+        //Koin
+        startKoin {
+            androidContext(this@App)
+
+            modules(convertersModule, interactorModule, networkModule, repositoryModule, storageModule, viewModelModule)
+        }
+
+        // 🧼 Ловим крэш-ошибки
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            Log.e("UncaughtException", "Uncaught exception in thread ${thread.name}", throwable)
+        }
+    }
+}
