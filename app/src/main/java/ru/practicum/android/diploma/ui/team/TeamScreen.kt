4e2ae0739b419c58ce5152191f26b4ddc0bd9a21
Ecballium elaterium <amanzholaimov@gmail.com>
2025-12-09 05:50:39 +0500

    fix detekt for import order

--- app/src/main/java/ru/practicum/android/diploma/ui/team/TeamScreen.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/team/TeamScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -13,7 +14,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.material3.MaterialTheme
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.ui.components.DisplayTitle
 import ru.practicum.android.diploma.ui.components.Heading
